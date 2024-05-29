95e2991ea8156ac9f7a65aaeda6b433088e0658e
Lais Almeida <laisdev25@gmail.com>
2024-05-29 10:52:16 -0300

    VISITA 1 - Inicio da API

---
+++ spec/requests/api/inn_api_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Inn API" do
+  context "GET api/v1/inn" do
+    it "success" do
+      #ARRANGE
+
+      #ACT
+
+      #ASSERT
+
+    end
+  end
+end
+
+
+=begin
+
+> API 1 endpoint
+formato json
+parametro: cnpj
+retorna: nome, cnpj, descricao da pousada, endereço completo, quantidade de quartos
+
+=end
